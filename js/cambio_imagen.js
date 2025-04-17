ae2409c9179c72b3a481882a865b8180e5fc8da0
EdNav55 <lalitonaba0505@gmail.com>
2025-04-17 12:39:37 -0600

    Proyecto terminado para ventana grande

--- js/cambio_imagen.js
+++ js/cambio_imagen.js
@@ -1,42 +1,35 @@
-function funcionesGaleria(ruta_imagenes, id) {
-    const m = ruta_imagenes.length;
-    let indice = 0;
-    const imagenes = document.querySelectorAll(`#${id} img`);
+function funcionesGaleria(id) {
     const btnAnterior = document.querySelector(`#${id} .btn-anterior`);
     const btnSiguiente = document.querySelector(`#${id} .btn-siguiente`);
-    const contenedorGaleria = document.getElementById(id);
+    const parteVisible = document.getElementById(id);
+    const contenedorGaleria = document.querySelector(`#${id} div`);
 
-    let desplazamiento = 0;
     let intervaloDesplazamiento;
-    const velocidadDesplazamiento = 10;
-
-    function actualizarDesplazamiento(direccion) {
-        imagenes.forEach(img => {
-            const currentTranslateX = parseFloat(img.style.transform.replace('translateX(', '').replace('px)', '') || 0);
-            img.style.transform = `translateX(${currentTranslateX + direccion}px)`;
-        });
-    }
+    const velocidadDesplazamiento = 8;
 
     function iniciarDesplazamiento(direccion) {
         clearInterval(intervaloDesplazamiento);
         intervaloDesplazamiento = setInterval(() => {
-            actualizarDesplazamiento(direccion);
-        }, 20);
+            const contenedorGaleriaRect = contenedorGaleria.getBoundingClientRect();
+            const parteVisibleRect = parteVisible.getBoundingClientRect();
+    
+            // Indica los pixeles que se ha modivo hacia la izquierda o derecha
+            const currentTranslateX = parseFloat(contenedorGaleria.style.transform.replace('translateX(', '').replace('px)', '') || 0);
+
+            const dentroLimites =
+                (contenedorGaleriaRect.left + direccion <= parteVisibleRect.left &&
+                    contenedorGaleriaRect.right + direccion >= parteVisibleRect.right);
+
+            if (dentroLimites) {
+                contenedorGaleria.style.transform = `translateX(${currentTranslateX + direccion}px)`
+            } else {
+                detenerDesplazamiento();
+            }
+        }, 15);
     }
 
     function detenerDesplazamiento() {
         clearInterval(intervaloDesplazamiento);
-    }
-
-    function modulo(n) {
-        return ((n % m) + m) % m;
-    }
-
-    function cambioImagenes() {
-        contador = 0;
-        imagenes.forEach(img => {
-            img.src = ruta_imagenes[modulo(indice + contador)];
-        });
     }
 
     btnAnterior.addEventListener("mousedown", () => {
@@ -56,26 +49,7 @@ function funcionesGaleria(ruta_imagenes, id) {
     btnSiguiente.addEventListener("mouseleave", detenerDesplazamiento);
 }
 
-const imagenes_puertas = [
-    "imagenes/productos/puerta_1.jpg",
-    "imagenes/productos/puerta_2.jpg",
-    "imagenes/productos/puerta_3.jpg",
-    "imagenes/productos/puerta_4.jpg",
-    "imagenes/productos/puerta_5.jpg",
-    "imagenes/productos/puerta_6.jpg",
-    "imagenes/productos/puerta_7.jpg",
-    "imagenes/productos/puerta_8.jpg",
-    "imagenes/productos/puerta_9.jpg"
-]
-
-funcionesGaleria(imagenes_puertas, "galeria_puertas");
-
-function cambioImagen() {
-    const contenedorGaleria = document.getElementById(`galeria_puertas`);
-    const primerHijo = contenedorGaleria.children[1];
-    const ultimoHijo = contenedorGaleria.children[6];
-
-    console.log(contenedorGaleria.offsetWidth);
-    console.log(primerHijo.offsetWidth);
-    primerHijo.style.transform = `translateX(${contenedorGaleria.offsetWidth}px)`;
-}
+funcionesGaleria("galeria_puertas");
+funcionesGaleria("galeria_ventanas");
+funcionesGaleria("galeria_canceles");
+funcionesGaleria("galeria_otros");
